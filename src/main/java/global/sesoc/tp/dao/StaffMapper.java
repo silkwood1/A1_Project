c5c55c8ded78f59637e40429826084187fe424d4
Unknown <1231wodnjs@naver.com>
2018-04-10 11:48:32 +0900

    StaffMapper
    staffMapper 수정

--- src/main/java/global/sesoc/tp/dao/StaffMapper.java
+++ src/main/java/global/sesoc/tp/dao/StaffMapper.java
@@ -6,5 +6,14 @@ import global.sesoc.tp.vo.StaffVO;
  * 직원
  */
 public interface StaffMapper {
+	int staff_profile(StaffVO vo);		//staff_profile 테이블
+	int staff_list(StaffVO vo);			//staff_list 테이블
+
+	String login(StaffVO vo);			//로그인
+	
+	int update_staff(StaffVO vo);		//스텝 정보수정
+	int update_staff_profile(StaffVO vo);	//수정
+	
+	
 
 }
